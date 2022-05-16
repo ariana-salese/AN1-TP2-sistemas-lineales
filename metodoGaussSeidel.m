ab8ed0e62e3061e24ce31d8892a493c0205402a1
MarceAriel99 <mrondan@fi.uba.ar>
2022-05-16 16:39:00 -0300

    Gauss Seidel terminado, fix en matriz inicial

--- metodoGaussSeidel.m
+++ metodoGaussSeidel.m
@@ -1,18 +1,18 @@
-function vector_resultado = metodoGaussSeidel(ds)
+function vector_resultado = metodoGaussSeidel(A, b)
   
-  res = [-0.01; -0.01; -0.01; -0.01; -0.01; -0.01];
+  res = [0; 0; 0; 0; 0; 0];
+  res_inicial = res;
+  n = rows(A);
   
-  for i = 0:20
+  do
+    for j = 1:n      
+      res(j,1)=(b(j)-(A(j,1:j-1)*res(1:j-1))-(A(j,j+1:n)*res(j+1:n)))/A(j,j);
+    endfor
+    
+  error = norm(res - res_inicial, inf);
+  res_inicial = res;
   
-    res(1,1) = 10 + 25 * res(2,1) + 50 * res(3,1);
-    res(2,1) = (25 * res(1,1) + res(3,1) + 30 * res(4,1)) / 56;
-    res(3,1) = (50 * res(1,1) + res(2,1) + 55 * res(4,1)) / 106;
-    res(4,1) = (6 * res(2,1) + 11 * res(3,1) + 5 * res(5,1) + 10 * res(6,1)) / 32;
-    res(5,1) = (25 * res(4,1) + res(6,1)) / 56;
-    res(6,1) = (50 * res(4,1) + res(5,1)) / 106;
-  
-  endfor
- 
+  until (error < 0.0001)
+
   vector_resultado = res;
-  
 endfunction
