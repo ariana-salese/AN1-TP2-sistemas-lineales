75af12c57fd6155778aa7b36eef7de93564e1694
ariana-salese <asalese@fi.uba.ar>
2022-05-19 15:51:48 -0300

    grafico voltaje segun hora

--- inversa.m
+++ inversa.m
@@ -7,7 +7,7 @@ function matriz_inversa = inversa(A, ds)
   matriz_inversa = zeros(n, n);
   
   for i = 1:n
-    matriz_inversa(:, i) = eliminacionDeGauss(A, identidad(:,i), ds);
+    matriz_inversa(:, i) = eliminacion_de_gauss(A, identidad(:,i), ds);
   endfor
   
 endfunction
--- main.m
+++ main.m
@@ -16,7 +16,7 @@ disp('----RESULTADO A-----');
 disp('');
 
 disp('6 decimales de presicion')
-disp(eliminacionDeGauss(A, b, 6));
+disp(eliminacion_de_gauss(A, b, 6));
 
 
 #-------------------------------------------- PUNTO B ------------------------------------------------------------
@@ -26,9 +26,9 @@ disp('----RESULTADO B-----');
 disp('');
 
 disp('3 decimales de presicion')
-disp(metodoLU(A, b, 3));
+disp(metodo_LU(A, b, 3));
 disp('6 decimales de presicion')
-disp(metodoLU(A, b, 6));
+disp(metodo_LU(A, b, 6));
 
 #-------------------------------------------- PUNTO C ------------------------------------------------------------
 
@@ -48,7 +48,7 @@ disp('');
 disp('----RESULTADO D-----');
 disp('');
 
-disp(metodoGaussSeidel(A, b));
+disp(metodo_gauss_seidel(A, b));
 
 #-------------------------------------------- PUNTO E ------------------------------------------------------------
 
@@ -57,7 +57,7 @@ disp('----RESULTADO E-----');
 disp('');
 
 voltajes = [0 10; 3 10.20; 6 10.30; 9 10.05; 12 9.95; 15 9.85; 18 9.80; 21 10.05; 24 10.15]
-voltajes_res = interpolacionLineal(voltajes);
+voltajes_res = interpolacion_lineal(voltajes);
 disp(voltajes_res)
 
 b_matriz = zeros(rows(b), rows(voltajes_res));
@@ -66,9 +66,21 @@ for i = 1:rows(voltajes_res)
   b_matriz(1,i) = voltajes_res(i,2);
 endfor
 
-disp(metodoLU(A, b_matriz, 6))
+disp(metodo_LU(A, b_matriz, 6));
 
+function graficar_voltajes(horas, voltajes)
+  
+  plot(horas, voltajes);
+  
+  title("Voltaje segˆyn hora");
+  xlabel ("Hora (h)");
+  ylabel ("Voltaje (V)");
+  grid on
+  set(0, "defaulttextfontsize", 24);
+  set(0, "defaultaxesfontsize", 16);
 
+  print -djpg voltajes_segun_hora.jpg 
+  
+endfunction
 
-
-
+graficar_voltajes(voltajes(:, 1), voltajes(:, 2));
